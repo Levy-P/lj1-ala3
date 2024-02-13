cca25042efb7138f93ab6050e6042938bea7648d
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-02-13 13:45:59 +0100

    add alle pagina templates

---
+++ gallery.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Gallery - Piazo's Pizza</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="Website for Piazo's Pizza">
+    <meta name="author" content="">
+    <meta name="keywords" content="Piazo's Pizza, Pizza, Italian Food, Italian Kitchen, Yummy, Restaurant">
+
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,regular,500,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="global.css">
+
+</head>
+
+<body>
+<?php include 'libs/components/header.php' ?>
+    <main id="gallery">
+
+    </main>
+<?php include 'libs/components/footer.php' ?>
+</body>
+</html>
