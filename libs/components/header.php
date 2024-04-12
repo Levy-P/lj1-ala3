125f83d422283dad336c9690fbf26aff126490e7
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-04-12 20:48:42 +0200

    change header and footer PHP syntax

--- libs/components/header.php
+++ libs/components/header.php
@@ -1,29 +1,27 @@
-<?php
-
+<!--
 // TODO - Verander de link EN id van Jobs naar de vacatures pagina (engels)
 // ? Drinks pagina staat op our menu. Als je klaar ben, verwijder deze comments.
+-->
 
-echo "
 <header flex>
-    <script src=\"libs/dropdown.js\" type=\"text/javascript\" defer></script>
-    <img src=\"assets/icons/hamburger.png\" alt=\"Dropdown Menu\" class=\"dropdown\" onclick=\"toggleNav();\">
-    <article flex id=\"nav\">
-        <a href=\"index.php\" id=\"nav-home\" flex><p>Home</p></a>
+    <script src="libs/dropdown.js" type="text/javascript" defer></script>
+    <img src="assets/icons/hamburger.png" alt="Dropdown Menu" class="dropdown" onclick="toggleNav();">
+    <article flex id="nav">
+        <a href="index.php" id="nav-home" flex><p>Home</p></a>
         <p>•</p>
-        <a href=\"ourmenu.php\" id=\"nav-ourmenu\" flex><p>Our Menu</p></a>
+        <a href="ourmenu.php" id="nav-ourmenu" flex><p>Our Menu</p></a>
         <p>•</p>
-        <a href=\"deals.php\" id=\"nav-deals\" flex><p>Deals</p></a>
+        <a href="deals.php" id="nav-deals" flex><p>Deals</p></a>
         <p>•</p>
-        <a href=\"booking.php\" id=\"nav-booking\" flex><p>Booking</p></a>
+        <a href="booking.php" id="nav-booking" flex><p>Booking</p></a>
         <p>•</p>
-        <a href=\"reviews.php\" id=\"nav-reviews\" flex><p>Reviews</p></a>
+        <a href="reviews.php" id="nav-reviews" flex><p>Reviews</p></a>
         <p>•</p>
-        <a href=\"gallery.php\" id=\"nav-gallery\" flex><p>Gallery</p></a>
+        <a href="gallery.php" id="nav-gallery" flex><p>Gallery</p></a>
         <p>•</p>
-        <a href=\"#\" id=\"nav-ID\" flex><p>Jobs</p></a>
+        <a href="#" id="nav-ID" flex><p>Jobs</p></a>
         <p>•</p>
-        <a href=\"aboutus.php\" id=\"nav-aboutus\" flex><p>About Us</p></a>
+        <a href="aboutus.php" id="nav-aboutus" flex><p>About Us</p></a>
     </article>
-    <figure id=\"logo\"></figure>
+    <figure id="logo"></figure>
 </header>
-";
