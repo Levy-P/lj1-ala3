e7171c5a16a8c49c496c08014d63a5b7933ac6bc
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-04-12 10:55:14 +0200

    add new pages

---
+++ reviews.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Piazo's Pizza</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0">
+    
+    <meta name="description" content="Website for Piazo's Pizza">
+    <meta name="author" content="Levy Pondman">
+    <meta name="keywords" content="Piazo's Pizza, Pizza, Italian Food, Italian Kitchen, Yummy, Restaurant">
+
+    <?php include 'libs/components/style.php' ?>
+</head>
+
+<body id="reviews">
+<?php include 'libs/components/header.php' ?>
+    <main>
+
+    </main>
+<?php include 'libs/components/footer.php' ?>
+</body>
+</html>
