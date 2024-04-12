8a1bd66eb122c29d8db2201f82712e43e7cb1c4f
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-04-12 15:48:41 +0200

    add deals/arragements page

--- deals.php
+++ deals.php
@@ -17,16 +17,28 @@
     <main>
         <section grid>
             <article grid class="arrangment">
-                <h1>A</h1>
+                <span>
+                    <h1>Family Dining</h1></span>
+                </span>
+                <p>15% Discount for Families during the weekend.</p>
             </article>
             <article grid class="arrangment">
-                <h1>B</h1>
+                <p>Kids under 12 eat free on weekdays.</p>
+                <span>
+                    <h1>Kids eat Free!</h1>
+                </span>
             </article>
             <article grid class="arrangment">
-                <h1>C</h1>
+            <p>Shared dining for large groups of people. Ranges from 5 to 24 people.</p>
+                <span>
+                    <h1>Shared Dining Experience</h1>
+                </span>
             </article>
             <article grid class="arrangment">
-                <h1>D</h1>
+                <span>
+                    <h1>3-Course Meal Including Drinks</h1>
+                </span>
+                <p>3-Course dining from our selected menu for 17.50$ per person</p>
             </article>
         </section>
     </main>
